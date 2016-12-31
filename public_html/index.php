6444bd5ebeac9abab0e7b0c2740208a268082f00
Maria Rosado <mrosado2@cnm.edu>
2016-12-30 22:44:04 -0700

    Will test the look with images

---
+++ public_html/index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- ADD YOUR CUSTOM CSS HERE -->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<![endif]-->
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/jquery-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src="https://www.google.com/recaptcha/api.js"></script>
+
+		<!-- FONT AWESOME -->
+		<script src="https://use.fontawesome.com/ccafd2629e.js"></script>
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+		<title>BlackCat Web development</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<!-- begin header and navbar -->
+			<header>
+				<nav class="nav-scroll navbar navbar-default navbar-fixed-top">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<a class="navbar-brand page-scroll">XXX Barbershop</a>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-left">
+								<li><a class="page-scroll" href="#home">Home</a></li>
+								<li><a class="page-scroll" href="#about">About Me</a></li>
+								<li><a class="page-scroll" href="#portfolio">Styles</a></li>
+								<li><a class="page-scroll" href="#contact">Contact Me</a> </li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</header>
+			<!-- begin welcome section -->
+			<main>
+				<!--<div id="page-top class="row" -->
+				<div class="col-xs-12">
+					<div id="home" class="jumbotron text-center">
+						<h1>XXX Barbershop</h1>
+						<h2>The Best Parber Parlor in Town</h2>
+						<br>
+						<i class="fa fa-angle-down fa-5x" aria-hidden="true"></i>
+
+					</div>
+					<!--/div>
+				</div>-->
+
+					<!----------------------- Begin photo/text section ---------------------------------------->
+
+					<a name="about"></a><!-- this is an "anchor" link -->
+					<section class="bg-gray">
+						<div class="container">
+							<div class="row">
+								<div class="col-sm-3">
+									<img src="../public_html/img/barber.png"
+										  class="img-responsive img-thumbnail xs-center-block m-b-2">
+								</div>
+								<div class="col-xs-9">
+									<div class="About me"></div><h2>Best Parlor Best Service</h2>
+									<p class="lead">We get the Best on style to make you look and feel great!
+									</p>
+									<p>We have xxx years experience, serving the Metro area
+										Our customer satisfaction is our number one priority.</p>
+									<p>.</p>
+								</div>
+							</div><!-- /.row -->
+						</div><!-- /.container -->
+					</section>
+
+					<!-- begin 4 column section -->
+
+					<a name="moar"></a><!-- this is an "anchor" link -->
+					<section>
+						<div class="container">
+							<div class></div>
+							<div class="jumbotron text-center">
+								<div class="text-center m-b-2"></div><h2>Styles</h2>
+								<div class="text-Styles"
+								<div class="row">
+								</div>
+							</div>
+						</div>
+					</section>
+					<div class="col-sm-3">
+						<div class="text-center-m-b-2">
+							<img src="../public_html/img/redhaircut.png" alt="style xxx" class="img-responsive img-circle img-thumbnail">
+							<h5>Aaaa-capstone</h5>
+							<p class="text-center m-b-2">Style</p>
+						</div>
+					</div><!-- /.col-sm-3 -->
+					<div class="col-sm-3">
+						<div class="text-center m-b-2">
+							<img src="../public_html/img/soldier-style-haircut.png" alt="soldier style"
+								  class="img-responsive img-circle img-thumbnail">
+							<div>
+								<h3>Title 2</h3>
+								<p class="text-center m-b-2">Project</p>
+							</div>
+						</div>
+					</div><!-- /.col-sm-3 -->
+
+					<div class="col-sm-3">
+						<div class="text-center m-b-2">
+							<img src="../public_html/img/curly-hair-cut.png" alt="curly hair cut"
+								  class="img-responsive img-circle img-thumbnail">
+							<div>
+								<h3>Title 3</h3>
+								<p class="text-center m-b-2">Project</p>
+							</div>
+						</div>
+					</div><!-- /.col-sm-3 -->
+					<div class="col-sm-3">
+						<div class="text-center m-b-2">
+							<img src="../public_html/img/haircut-model.png" alt="haircut model"
+								  class="img-responsive img-circle img-thumbnail">
+							<div>
+								<h3>Title 4</h3>
+								<p class="text-center m-b-2">Style</p>
+							</div>
+						</div>
+					</div><!-- /.col-sm-3 -->
+				</div>
+		</div><!-- /.container -->
+		<!-- /.sfooter-content -->
+		<!------------------------- CONTACT FORM ------------------------------------->
+		<div id="contact" class="container section">
+			<h2 class="heading">Contact Me</h2>
+			<div class="row">
+				<div class="col-md-3"></div>
+				<!--Begin Contact Form-->
+				<div class="col-md-6">
+					<form id="contact-form" action="php/mailer.php" method="POST" novalidate>
+						<div class="form-group">
+							<label for="name">Name <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-user" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="email">Email <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+								</div>
+								<input type="email" class="form-control" id="email" name="email" placeholder="Your Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="subject">Subject</label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="message">Message <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-comment" aria-hidden="true"></i>
+								</div>
+								<textarea class="form-control" rows="5" id="message" name="message"
+											 placeholder="Message (2000 characters max)"></textarea>
+							</div>
+						</div>
+
+						<!-- reCAPTCHA -->
+						<div class="g-recaptcha" data-sitekey="6LdVDA8UAAAAAHEKILbduVUL7Qu1wzRB9gwom5hT"></div>
+
+						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+					</form>
+				</div>
+				<div class="col-md-3"></div>
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+						<div id="output-area"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- begin footer -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<!--TWITTER ICON-->
+						<a href="//twitter.com/BlackcatDevelop" target="_blank" class="btn btn-social-icon btn-twitter">
+							<span class="fa fa-twitter fa-2x"></span>
+						</a>
+						<!--GITHUB ICON-->
+						<a href="//github.com/mrosado2" target="_blank" class="btn btn-social-icon btn-github">
+							<span class="fa fa-github fa-2x"></span>
+						</a>
+						<!--LINKEDIN ICON-->
+						<a href="//https://www.linkedin.com/in/maria-rosado-bb1608132?trk=hp-identity-name" target="_blank"
+							class="btn btn-social-icon btn-linkedin">
+							<span class="fa fa-linkedin fa-2x"></span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
